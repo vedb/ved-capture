6f8c1647529dd0a15e80f4a1aeb214189c061223
phausamann <peter@hausamann.de>
2020-03-24 18:57:30 +0100

    Add update subcommand Add test_cli.py

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # ved-capture
+
+**ved-capture** is the app for simultaneous recording of camera, gaze and head
+ tracking data for the Visual Experience Database.
+ 
+## Installation
+
+The app can be installed on ubuntu 18.04 with a single Python script. 
+
+    $ python3 install_ved_capture.py
+    
+The script will guide you through the setup process and instruct you what to 
+ do. Since the app isolates most of it's dependencies in a dedicated
+ environment, the installation has a size of about 3.6 GB, so make sure you 
+ have enough space.
+ 
+## Usage
+
+The central tool of this app is the command line tool `vedc`. You can use it
+ to generate recording configuration, make recordings and update the app.
+ 
+### Generating a configuration
+
+Plug in your hardware (Pupil core system, RealSense T265, FLIR camera) and run:
+
+    $ vedc generate_config
+    
+This will create a config file `~/.config/vedc/config.yaml` that will work
+ for your current setup. You can adjust some of the parameters in this file, 
+ such as `fps` and `resolution`.
+
+### Recording
+
+Once you have generated a configuration, run:
+
+    $ vedc record
+    
+to start recording. 
+
+By default, the data is recorded to `~/recordings/<today>/<no>`. After 
+ recording, the data can be loaded into Pupil Player for inspection.
+  
+### Updating
+
+If an update is available, the `vedc record` command will tell you to update. 
+ To do this, run:
+ 
+    $ vedc update
--- ved_capture/cli.py
+++ ved_capture/cli.py
@@ -1,18 +1,21 @@
 """ ``vec_capture.cli`` bundles a variety of command line interfaces. """
-
 import logging
 import inspect
 import importlib
 import traceback
 
 import click
+from git.exc import GitError
 
 from pupil_recording_interface import MultiStreamRecorder
 
+from ved_capture._version import __version__
 from ved_capture.config import ConfigParser, save_metadata
+from ved_capture.utils import get_paths, update_repo, update_environment
+from ved_capture import utils
 
 
-def _init_logger(subcommand, verbose=False):
+def init_logger(subcommand, verbose=False):
     """"""
     logger = logging.getLogger(__name__ + ":" + subcommand)
     logger.setLevel(logging.DEBUG)
@@ -27,20 +30,32 @@ def _init_logger(subcommand, verbose=False):
     stream_handler.setFormatter(stream_formatter)
     logger.addHandler(stream_handler)
 
+    # TODO this can be done more elegantly
+    utils.logger = logger
+
     return logger
 
 
 @click.group("vedc")
+@click.version_option(version=__version__)
 def vedc():
-    """ vedc command line interface. """
+    """ Visual Experience Data Capture.
+
+    Command line tool for the Visual Experience Database.
+    """
 
 
 @click.command("record")
 @click.option(
     "-c", "--config-file", default=None, help="Path to recording config file.",
 )
-def record(config_file):
+@click.option(
+    "-v", "--verbose", default=False, help="Verbose output.", is_flag=True,
+)
+def record(config_file, verbose):
     """ Run recording. """
+    logger = init_logger(str(inspect.currentframe()), verbose=verbose)
+
     config_parser = ConfigParser(config_file)
 
     metadata = config_parser.get_metadata()
@@ -55,13 +70,60 @@ def record(config_file):
     recorder.run()
 
 
+@click.command("update")
+@click.option(
+    "-v", "--verbose", default=False, help="Verbose output.", is_flag=True,
+)
+@click.option(
+    "-l",
+    "--local",
+    default=False,
+    help="Update from local repository.",
+    is_flag=True,
+)
+@click.option(
+    "-s", "--stash", default=False, help="Stash local changes.", is_flag=True,
+)
+def update(verbose, local, stash):
+    """ Update installation. """
+    logger = init_logger(str(inspect.currentframe()), verbose=verbose)
+
+    paths = get_paths()
+    if paths is None:
+        raise click.ClickException(
+            "Application paths have not been set up. You might need to "
+            "reinstall."
+        )
+
+    # update repo if needed
+    if not local:
+        logger.info(f"Updating {paths['vedc_repo_folder']}")
+        try:
+            update_repo(paths["vedc_repo_folder"], stash)
+        except GitError as e:
+            raise click.ClickException(
+                f"Repository update failed. Reason: {str(e)}"
+            )
+
+    # update environment
+    logger.info("Updating environment.\nThis will take a couple of minutes. ☕")
+    return_code = update_environment(
+        paths["conda_binary"],
+        paths["conda_script"],
+        paths["vedc_repo_folder"],
+        local=local,
+    )
+    if return_code != 0:
+        raise click.ClickException("Environment update failed")
+
+
 @click.command("check_install")
 @click.option(
     "-v", "--verbose", default=False, help="Verbose output.", is_flag=True,
 )
 def check_install(verbose):
     """ Test installation. """
-    logger = _init_logger(str(inspect.currentframe()), verbose=verbose)
+    logger = init_logger(str(inspect.currentframe()), verbose=verbose)
 
     failures = []
 
@@ -84,4 +146,5 @@ def check_install(verbose):
 
 # add subcommands
 vedc.add_command(record)
+vedc.add_command(update)
 vedc.add_command(check_install)
