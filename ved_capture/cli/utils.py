794dc9fff14bf6330eb970c28e7a8a18ce1b02ef
Peter Hausamann <peter.hausamann@tum.de>
2020-04-28 20:16:06 +0200

    Add terminal refresh method

--- ved_capture/cli/utils.py
+++ ved_capture/cli/utils.py
@@ -89,13 +89,53 @@ def init_logger(
     return logging.getLogger("vedc." + subcommand)
 
 
-def print_log_buffer(stream):
-    """ Print buffered logs. """
+def flush_log_buffer(stream):
+    """ Flush buffered logs. """
     stream.flush()
     buffer = stream.getvalue()
     stream.truncate(0)
     if len(buffer):
+        return buffer
+
+
+def print_log_buffer(stream):
+    """ Print buffered logs. """
+    buffer = flush_log_buffer(stream)
+    if buffer:
         print(buffer)
+
+
+def refresh(t, stream_buffer, status_buffer, timeout=0.1, num_empty_lines=1):
+    """ Refresh terminal output and return user input. """
+    if not hasattr(refresh, "first_log_line"):
+        refresh.first_log_line = t.get_location()[0]
+
+    # print stream buffer
+    if stream_buffer is not None:
+        print(
+            t.move_xy(0, refresh.first_log_line) + t.clear_eos + stream_buffer
+        )
+    else:
+        print(t.move_xy(0, refresh.first_log_line) + t.clear_eos + t.move_up)
+
+    # check bottom offset
+    num_status_lines = len(status_buffer.splitlines()) + 1
+    refresh.first_log_line = t.get_location()[0]
+    actual_offset = t.height - t.get_location()[0]
+    desired_offset = num_status_lines + num_empty_lines
+
+    # adjust if necessary
+    if desired_offset > actual_offset:
+        refresh.first_log_line -= desired_offset - actual_offset
+        print("\n" * (desired_offset - 2))
+
+    # print status buffer
+    first_status_line = t.height - num_status_lines
+    print(t.move_y(first_status_line) + status_buffer)
+
+    # wait for keypress
+    with t.cbreak():
+        return t.inkey(timeout)
 
 
 def raise_error(msg, logger=None):
