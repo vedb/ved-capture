c17cf6d96e40a82b503318215987584a469102c4
Peter Hausamann <peter.hausamann@tum.de>
2020-05-19 09:42:08 +0200

    Add edit_config command

--- ved_capture/cli/__init__.py
+++ ved_capture/cli/__init__.py
@@ -6,7 +6,7 @@ from ved_capture._version import __version__
 from ved_capture.cli.app import update, check_install, save_logs
 from ved_capture.cli.calibrate import calibrate
 from ved_capture.cli.cam_params import estimate_cam_params
-from ved_capture.cli.config import generate_config
+from ved_capture.cli.config import generate_config, edit_config
 from ved_capture.cli.record import record
 from ved_capture.cli.show import show
 
@@ -26,6 +26,7 @@ vedc.add_command(calibrate)
 vedc.add_command(estimate_cam_params)
 vedc.add_command(show)
 vedc.add_command(generate_config)
+vedc.add_command(edit_config)
 vedc.add_command(update)
 vedc.add_command(check_install)
 vedc.add_command(save_logs)
