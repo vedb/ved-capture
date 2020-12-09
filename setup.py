4809a3d9de54af1f74d1ddeb9790032020792e83
Peter Hausamann <peter.hausamann@tum.de>
2020-12-09 12:35:48 +0100

    Bump version: 0.3.1 → 0.4.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requirements = [
 
 setup(
     name="ved-capture",
-    version="0.3.1",
+    version="0.4.0",
     packages=find_packages(),
     long_description=open("README.md").read(),
     entry_points="""
