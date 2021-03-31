77c1daca32065cf0416af1e779ca62e91b8e2a07
Peter Hausamann <peter.hausamann@tum.de>
2021-03-31 14:11:27 +0200

    Update ConfigParser tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -54,7 +54,56 @@ class TestConfigParser:
             == "test_site"
         )
 
-    def test_get_folder(self, parser, parser_minimal, config_dir):
+    def test_get_stream_config(self, parser, parser_override):
+        """"""
+        # single updated value
+        assert parser.get_stream_config("video", "eye0", "fps") == 200
+
+        # whole dict
+        assert set(parser.get_stream_config("video", "eye0").keys()) == {
+            "device_type",
+            "device_uid",
+            "resolution",
+            "fps",
+            "color_format",
+            "exposure_mode",
+            "controls",
+        }
+
+        # override
+        assert set(
+            parser_override.get_stream_config("video", "t265").keys()
+        ) == {
+            "device_type",
+            "device_uid",
+            "resolution",
+            "fps",
+            "color_format",
+        }
+
+    def test_get_command_config(self, parser, parser_override):
+        """"""
+        # single updated value
+        assert parser.get_command_config("record", "policy") == "overwrite"
+
+        # whole dict
+        assert set(parser.get_command_config("record", "motion").keys()) == {
+            "odometry",
+            "accel",
+            "gyro",
+        }
+
+        # override
+        assert set(parser_override.get_command_config("record").keys()) == {
+            "video",
+            "motion",
+            "folder",
+            "policy",
+        }
+
+    def test_get_folder(
+        self, parser, parser_minimal, parser_override, config_dir
+    ):
         """"""
         import datetime
 
@@ -72,26 +121,32 @@ class TestConfigParser:
         )
 
         # override
+        folder = parser_override.get_folder("record")
+        assert folder == Path.home() / "recordings" / "vedc_test"
+
+        # user specified
         folder = parser.get_folder("record", folder=config_dir)
         assert folder == config_dir
 
-    def test_get_policy(self, parser, parser_minimal):
+    def test_get_policy(self, parser, parser_minimal, parser_override):
         """"""
         # test config file
         assert parser.get_policy("record") == "overwrite"
-        # user override
-        assert parser.get_policy("record", "new_folder") == "new_folder"
         # minimal/package default
         assert parser_minimal.get_policy("record") == "here"
+        # override
+        assert parser_override.get_policy("record") == "overwrite"
+        # user specified
+        assert parser.get_policy("record", "new_folder") == "new_folder"
 
     def test_get_show_video(self, parser, parser_minimal):
         """"""
         # test config file
         assert parser.get_show_video()
-        # user override
-        assert not parser.get_show_video(False)
         # minimal/package default
         assert not parser_minimal.get_show_video()
+        # user specified
+        assert not parser.get_show_video(False)
 
     def test_get_recording_cam_params(self, parser_minimal):
         """"""
@@ -101,7 +156,7 @@ class TestConfigParser:
             ["world", "t265"],
         )
 
-    def test_get_metadata(self, parser, monkeypatch):
+    def test_get_metadata(self, parser, parser_override, monkeypatch):
         """"""
         # as list
         monkeypatch.setattr("builtins.input", lambda x: "000")
@@ -114,6 +169,9 @@ class TestConfigParser:
         monkeypatch.setattr("builtins.input", lambda x: "001")
         assert parser.get_metadata() == {"subject_id": "001"}
 
+        # override
+        assert parser_override.get_metadata() == {}
+
     def test_get_recording_configs(self, parser):
         """"""
         # TODO add __eq__ to Config to handle equality check
@@ -121,12 +179,17 @@ class TestConfigParser:
 
         assert config_list[0].stream_type == "video"
         assert config_list[0].device_type == "flir"
+        assert config_list[0].device_uid == "flir"
         assert config_list[0].resolution == (2048, 1536)
         assert config_list[0].pipeline[0].process_type == "video_recorder"
         assert config_list[0].pipeline[1].process_type == "video_display"
 
+        assert config_list[1].fps == 200
+        assert config_list[2].fps == 200
+
         assert config_list[4].stream_type == "motion"
         assert config_list[4].device_type == "t265"
+        assert config_list[4].device_uid == "t265"
         assert config_list[4].pipeline[0].process_type == "motion_recorder"
 
     def test_get_calibration_configs(self, parser):
